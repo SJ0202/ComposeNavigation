60e9285d5a5f9780be46a765cc634911984ca86d
seongju <dnflrkrwhr5@naver.com>
2023-03-20 10:30:09 +0900

    create custom button textField

---
+++ app/src/main/java/com/seongju/composenavigation/ui/theme/Dimenstions.kt
@@ -0,0 +1,6 @@
+package com.seongju.composenavigation.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val SpaceSmall = 8.dp
+val SpaceDefault = 20.dp
--- app/src/main/java/com/seongju/composenavigation/ui/theme/Shape.kt
+++ app/src/main/java/com/seongju/composenavigation/ui/theme/Shape.kt
@@ -1,5 +1,6 @@
 package com.seongju.composenavigation.ui.theme
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
@@ -8,4 +9,15 @@ val Shapes = Shapes(
     small = RoundedCornerShape(4.dp),
     medium = RoundedCornerShape(4.dp),
     large = RoundedCornerShape(0.dp)
+)
+
+val BoxRoundShapes = RoundedCornerShape(
+    10.dp
+)
+
+val ButtonInnerPadding = PaddingValues(
+    start = 20.dp,
+    top = 10.dp,
+    bottom = 10.dp,
+    end = 20.dp
 )
